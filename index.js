9006480ec73b2ace84bb6b58fdea258563c64f08
James Langford <James Langford>
2016-10-07 23:20:41 +0100

    moreeee

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
+app.set('css', __dirname + '/public/stylesheets');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
