71af6218e5a1585696663ef3d7b5e4a6d47e9c47
tom <tom.connolly2511@gmail.com>
2016-10-28 02:57:39 +0100

    db connection added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var client;
+
+var client = require('redis').createClient(process.env.REDIS_URL);
+client.on('connect', function() {
+    console.log('connected.');
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,13 +16,26 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
 app.get('/db', function(request, response) {
   response.render('pages/db');
-})
+});
+
+app.get('/redis', function(request, response) {
+    
+    client.hmset('frameworks', 'javascript', 'AngularJS', 'css', 'Bootstrap', 'node', 'Express');
+
+    client.hgetall('frameworks', function(err, object) {
+        console.log(object);
+        response.render('pages/redis_test.ejs', {name : object} );
+    });
+    
+    
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
