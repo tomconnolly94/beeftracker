8abfed92ddfb97f3ff20f7e4608f7afec741e9a4
tom <tom.connolly2511@gmail.com>
2016-11-03 17:14:30 +0000

    changes

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var client;
-
-var client = require('redis').createClient(process.env.REDIS_URL);
-client.on('connect', function() {
-    console.log('connected.');
-});
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 app.set('port', (process.env.PORT || 5000));
+
+app.set()
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -25,17 +22,49 @@ app.get('/db', function(request, response) {
   response.render('pages/db');
 });
 
-app.get('/redis', function(request, response) {
-    
-    client.hmset('frameworks', 'javascript', 'AngularJS', 'css', 'Bootstrap', 'node', 'Express');
+/*app.get('/beef/:tagId', function(req, res) {
+  res.send("tagId is set to " + req.params.tagId);
+});*/
 
-    client.hgetall('frameworks', function(err, object) {
-        console.log(object);
-        response.render('pages/redis_test.ejs', {name : object} );
+app.get('/beef/:tagId', function(request, response) {
+    
+    console.log(process.env.MONGODB_URL);
+    
+    var beefId = request.params.tagId;
+    
+    MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
+        if(err){ console.log(err);}else{
+            
+            console.log("Connected correctly to server.");
+            
+            console.log(beefId);
+                
+            var cursor = db.collection("beef_data").find({beefId : beefId});
+            cursor.each(function(err, doc) {
+                assert.equal(err, null);
+                
+                if (doc != null) {
+                    response.render('pages/generic_beef.ejs', {name : doc.password});
+                } 
+                else{
+                    response.send("beef not found.")
+                }
+            });
+        
+            db.close();
+        }
     });
     
+    //response.render('pages/mongodb_test.ejs', {name : "hello"});
     
 });
+
+app.post('/adduser', function(req,res){
+    
+    
+    
+    
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
