c127baddbd4cfd62ab85c9da541999e700f0111f
tom <tom.connolly2511@gmail.com>
2016-12-09 01:13:00 +0000

    ajax for beef complete

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-
-var form = require("express-form")
-var field = form.field;
 // Put this statement near the top of your module
 var bodyParser = require('body-parser');
 // Put these statements before you define any routes.
@@ -13,16 +9,20 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use('/libraries', express.static(__dirname + '/node_modules/'));
+// ### Reference Routes ### 
 
-app.use('/controllers', express.static(__dirname + '/controllers'));
+//route to reference images
+app.use('/artist_images', express.static(__dirname + '/public/assets/images/artists/'));
+//route to reference libraries like angular
+app.use('/libraries', express.static(__dirname + '/node_modules/'));
+//route to reference controller scripts
+app.use('/controllers', express.static(__dirname + '/controllers/'));
+//route to reference css scripts
+app.use('/stylesheets', express.static(__dirname + '/public/stylesheets/'))
 
 app.get('/', function(request, response) {
   response.render('pages/splash');
@@ -55,23 +55,27 @@ app.get('/artist', function(request, response) {
 app.get('/search/:tagId', function(request, response) {
    console.log(process.env.MONGODB_URL);
 
-    var beefIdentifier = request.params.tagId;
+    var identifier = request.params.tagId;
 
     MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
         if(err){ console.log(err); }
         else{
-
-            var field_name = 'beefId';
-
-            var qry = "{\"" + field_name + "\":" + beefIdentifier + "}";
-
+            var field_name = 'event_id';
+            
+            /*
+            //code to create a qry string that matches NEAR to query string
+            var end = "{ \"$regex\": \"" + identifier + "\", \"$options\": \"i\" }";
+            var qry = "{ \"" + field_name + "\" : " + end + " }";*/
+            
+            var qry = "{ \"" + field_name + "\" : \"" + identifier + "\" }";
+            
             console.log(qry);
-
+            
             db.collection("event_data").find(JSON.parse(qry)).toArray(function(queryErr, docs) {
                 console.log("");
-                console.log("query: " + beefIdentifier);
+                console.log("query: " + identifier);
                 console.log("response: " + docs.length);
-                response.send({beefObject : docs[0]});
+                response.send({eventObject : docs[0]});
                 db.close()
             });
 
@@ -81,47 +85,8 @@ app.get('/search/:tagId', function(request, response) {
 });
 
 app.get('/beef/:tagId', function(request, response) {
-
    response.render('pages/beef_blended.ejs');
 });
-
-app.post('/gen_search', form(field("search_ref")), function(req, res){
-    if (!req.form.isValid) { console.log(req.form.errors); } //print error to console
-    else {
-        //connecto to DB
-        MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
-            if(err){ console.log(err); } //print error to console
-            else{
-
-                var field_name = 'aggressor';
-                var identifier = req.body.search_ref;
-                console.log(identifier);
-
-                //var qry = "{ \"" + field_name + "\" : \" + identifier + "\" }";
-
-                //var end = new RegExp('^'+identifier+'$', "i");
-                var end = "{ \"$regex\": \"" + identifier + "\", \"$options\": \"i\" }";
-
-                var qry = "{ \"" + field_name + "\" : " + end + " }";
-
-
-                console.log(qry);
-
-                db.collection("event_data").find(JSON.parse(qry)).toArray(function(queryErr, docs) {
-
-                    res.render('pages/search_results.ejs', {beefObject : docs});
-
-                });
-
-                db.close();
-            }
-        });
-    }
-});
-
-app.post('/adduser', function(req,res){
-
-})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
