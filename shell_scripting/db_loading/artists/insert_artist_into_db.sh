aec8e11a5bb5222d5425de4d28604753d9cd6530
tom <tom.connolly2511@gmail.com>
2016-12-22 22:42:16 +0000

    new changes - tom

--- shell_scripting/db_loading/artists/insert_artist_into_db.sh
+++ shell_scripting/db_loading/artists/insert_artist_into_db.sh
@@ -5,6 +5,9 @@ counter=0
 while read line; do
 
     data[$counter]=$line
+    
+    echo $line
+    echo $counter
 
     counter=$((counter+1))
 
@@ -15,15 +18,15 @@ count_command='db.artist_data.count( { } )'
 event_count=$( mongo ds141937.mlab.com:41937/heroku_w63fjrg6 -u tom -p tom --eval "$count_command";)
 event_count="${event_count:$length-1}" # take the last char off the end of the response
 event_count="$(($event_count + 1))" #add one to get the next artist_id
-to_string="${data[0]} ${data[1]}" #concat to string data
 
+echo ${data[4]}
 
 insert_cmd='db.artist_data.insert( { 
 "stage_name" : '\"${data[0]}\"', 
 "birth_name" : '\"${data[1]}\"',
-"d_o_b" : '\"${data[2]}\"',
+"d_o_b" : '${data[2]}',
 "img_link" : '\"${data[3]}\"', 
-"artist_id" : '\"$to_string\"', 
+"artist_id" : '\"$event_count\"', 
 "bio" : '\"${data[4]}\"'
 } );'
 
