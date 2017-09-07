4ea9a5f79751783a234b1cb0baf3b3eb9ebbe2c8
tom <tom.connolly2511@gmail.com>
2017-09-07 23:18:25 +0100

    update line counting program to only count code files

--- count_lines_of_code.sh
+++ count_lines_of_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-echo $(find . -name '*.*' | grep -vE '*.git*|node_modules|bower_components|*.json*|*.png*|*.jpg*|Procfile|npm-debug.log|*.md' | xargs wc -l | tail -1 | sed 's#\ total##g' | sed 's#\ ##g')
+echo $(find . -name '*.*' | grep -vE '*.git*|node_modules|bower_components|*.json*|*.png*|*.jpg*|Procfile|npm-debug.log|*.md|*.beefdata|*.db|*.ico|*.PNG')
+echo $(find . -name '*.*' | grep -vE '*.git*|node_modules|bower_components|*.json*|*.png*|*.jpg*|Procfile|npm-debug.log|*.md|*.beefdata|*.db|*.ico|*.PNG' | xargs wc -l | tail -1 | sed 's#\ total##g' | sed 's#\ ##g')
