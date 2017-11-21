5551f042f4f0613b427bdd532d9067895419bcdd
tom <tom.connolly2511@gmail.com>
2017-11-21 20:47:44 +0000

    stats page initial dev complete

--- public/controllers/admin/broken_fields_stats/broken_fields_stats_controller.js
+++ public/controllers/admin/broken_fields_stats/broken_fields_stats_controller.js
@@ -22,18 +22,70 @@ broken_fields_stats_app.controller("brokenFieldsStatsController", ['$scope','$ht
             $scope.data = response_1.data.data;
             console.log($scope.data);
             
-            $scope.sources = [];
+            $scope.fields = [
+                "title",
+                "relevant_actors",
+                "content",
+                "date",
+                "quotes",
+                "categories",
+                "img_link",
+                "special_feature"
+            ];
+            $scope.broken_reasons = [
+                "empty",
+                "incorrect_format",
+                "too_long",
+                "too_short",
+            ];
+            $scope.broken_reasons_headers = [
+                $scope.broken_reasons[0].charAt(0) + $scope.broken_reasons[0].charAt(1),
+                $scope.broken_reasons[1].charAt(0) + $scope.broken_reasons[1].charAt(10),
+                $scope.broken_reasons[2].charAt(0) + $scope.broken_reasons[2].charAt(4),
+                $scope.broken_reasons[3].charAt(0) + $scope.broken_reasons[3].charAt(4),
+            ];
+            $scope.threshold = 15;
             
-            for(var i = 0; i < $scope.data.length; i++){
+            $scope.build_stats = function(){
                 
-                var base_source = $scope.data[i].source.split("/")[2];
+                $scope.stats = {};
+                $scope.sources = [];
                 
-                if($scope.sources.indexOf(base_source) == -1){
-                    console.log(base_source);
-                    $scope.sources.push(base_source);
+                for(var i = 0; i < $scope.data.length; i++){
+
+                    var record = $scope.data[i];
+
+                    var base_source = record.source.split("/")[2];
+
+                    if($scope.sources.indexOf(base_source) == -1){
+                        $scope.sources.push(base_source);
+
+                        $scope.stats[base_source] = {};
+                        for(var j = 0; j < $scope.fields.length; j++){
+
+                            $scope.stats[base_source][$scope.fields[j]] = {};
+
+                            for(var k = 0; k < $scope.broken_reasons.length; k++){
+
+                                $scope.stats[base_source][$scope.fields[j]][$scope.broken_reasons[k]] = {};
+                                $scope.stats[base_source][$scope.fields[j]][$scope.broken_reasons[k]].value = 0;
+                                $scope.stats[base_source][$scope.fields[j]][$scope.broken_reasons[k]].bg_colour = "white";
+                                $scope.stats[base_source][$scope.fields[j]][$scope.broken_reasons[k]].incorrect_values = [];
+                            }
+                        }
+                    }
+                    else{
+                        $scope.stats[base_source][record.broken_field][record.broken_mode].value++;
+                        $scope.stats[base_source][record.broken_field][record.broken_mode].incorrect_values.push(record.value);
+
+                        if($scope.stats[base_source][record.broken_field][record.broken_mode].value >= $scope.threshold){
+                            $scope.stats[base_source][record.broken_field][record.broken_mode].bg_colour = "red";
+                        }
+                    }
                 }
             }
-            console.log($scope.sources);
+            $scope.build_stats();
+            console.log($scope.stats);
         }, 
         function(response_1) {
             //failed http request
