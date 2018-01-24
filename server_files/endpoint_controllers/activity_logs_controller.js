623d2f22951fc08d30aecac99053e2f61571f0d6
tom <tom.connolly2511@gmail.com>
2018-01-24 23:25:22 +0000

    testing driven development on activity log controller

--- server_files/endpoint_controllers/activity_logs_controller.js
+++ server_files/endpoint_controllers/activity_logs_controller.js
@@ -14,7 +14,32 @@ module.exports = {
                 var event_id_object = BSON.ObjectID.createFromHexString(event_id);
 
                 //standard query to match an event and resolve aggressor and targets references
-                db.collection(db_ref.get_activity_logs_table()).find({ event_id: event_id_object }).toArray(function(err, docs) {
+                db.collection(db_ref.get_activity_logs_table()).aggregate([{ $match: { event_id: event_id_object }},
+                                                                            { $lookup : { 
+                                                                                from: db_ref.get_user_details_table(),
+                                                                                localField: "user_id",
+                                                                                foreignField: "_id",
+                                                                                as: "user_object" }},
+                                                                           { $project: {
+                                                                               "action": 1,
+                                                                               "date": 1,
+                                                                               "likes": 1,
+                                                                               "user_object": {
+                                                                                   $let: {
+                                                                                       vars: {
+                                                                                           first_user: {
+                                                                                                "$arrayElemAt": [ "$user_object", 0 ]
+                                                                                           }
+                                                                                       },
+                                                                                       in: {
+                                                                                           first_name: "$$first_user.first_name",
+                                                                                           last_name: "$$first_user.first_name",
+                                                                                           img_title: "$$first_user.first_name"
+                                                                                       }
+                                                                                    }
+                                                                               }
+                                                                           }}
+                                                                          ]).toArray(function(err, docs) {
                     //handle error
                     if(err) { console.log(err);}
                     else{
@@ -40,7 +65,32 @@ module.exports = {
                 var actor_id_object = BSON.ObjectID.createFromHexString(actor_id);
 
                 //standard query to match an event and resolve aggressor and targets references
-                db.collection(db_ref.get_activity_logs_table()).find({ actor_id: actor_id_object }).toArray(function(err, docs) {
+                db.collection(db_ref.get_activity_logs_table()).aggregate([{ $match: { actor_id: actor_id_object }},
+                                                                            { $lookup : { 
+                                                                                from: db_ref.get_user_details_table(),
+                                                                                localField: "user_id",
+                                                                                foreignField: "_id",
+                                                                                as: "user_object" }},
+                                                                           { $project: {
+                                                                               "action": 1,
+                                                                               "date": 1,
+                                                                               "likes": 1,
+                                                                               "user_object": {
+                                                                                   $let: {
+                                                                                       vars: {
+                                                                                           first_user: {
+                                                                                                "$arrayElemAt": [ "$user_object", 0 ]
+                                                                                           }
+                                                                                       },
+                                                                                       in: {
+                                                                                           first_name: "$$first_user.first_name",
+                                                                                           last_name: "$$first_user.first_name",
+                                                                                           img_title: "$$first_user.first_name"
+                                                                                       }
+                                                                                    }
+                                                                               }
+                                                                           }}
+                                                                          ]).toArray(function(err, docs) {
                     //handle error
                     if(err) { console.log(err);}
                     else{
