4ef9915e413be43110a098bb808243eefa0c339a
tom <tom.connolly2511@gmail.com>
2018-02-04 15:12:50 +0000

    new schema file

---
+++ server_files/schemas/event_contribution_schema.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var event_contribution_schema = new Schema({
+    user: Schema.ObjectId,
+    date_of_submission: Date,
+    date_of_approval: Date,
+    contribution_details:[{
+        field: String,
+        addition: String,
+        removal: String
+    }]
+});
+
+module.exports.model = mongoose.model('EventContribution', event_contribution_schema);
+module.exports.schema = event_contribution_schema;
