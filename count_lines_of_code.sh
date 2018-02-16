294b8021a73fc93a8e19b66bc9a5ebd601917b65
tom <tom.connolly2511@gmail.com>
2018-02-16 18:07:09 +0000

    dependency restructuring and more file tidying

--- count_lines_of_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo $(find . -name '*.*' | grep -vE '*.git*|node_modules|bower_components|*.json*|*.png*|*.jpg*|Procfile|npm-debug.log|*.md|*.beefdata|*.db|*.ico|*.PNG|*.pyc')
-echo $(find . -name '*.*' | grep -vE '*.git*|node_modules|bower_components|*.json*|*.png*|*.jpg*|Procfile|npm-debug.log|*.md|*.beefdata|*.db|*.ico|*.PNG|*.pyc' | xargs wc -l | tail -1 | sed 's#\ total##g' | sed 's#\ ##g')
