4d7732ea9d9a38f396293fdf17e4af36bff73ca6
Tom Connolly <tom.connolly@protonmail.com>
2018-03-23 20:50:03 +0000

    updates to user controller, PUT handler needs a lot more work

--- server_files/endpoint_controllers/users_controller.js
+++ server_files/endpoint_controllers/users_controller.js
@@ -226,9 +226,126 @@ module.exports = {
         });
     },
     
-    updateUser: function(user_details, files, headers, callback){
-        console.log(user_details);
-        response.send({test: "endpoint not implemented yet."});
+    updateUser: function(user_details, files, headers, existing_object_id, callback){
+        
+                
+        if(err){ console.log(err); }
+        else{
+
+            var ip_address = null;
+            var password_data = { 
+                hashed_password: null,
+                salt: null
+            }
+
+            //if client provides an ip address, create new jsonwebtoken with it and store as cookie
+            if(headers && headers['x-forwarded-for']){
+                ip_address = headers['x-forwarded-for'];
+            }                     
+
+            //generate password if its provided
+            if(user_details.password){
+                password_data = hashing.hash_password(user_details.password);
+            }
+
+            var birth_date;
+
+            //prepare date of birth
+            if(user_details.d_o_b){
+                var split_birth_date = user_details.d_o_b.split("/");
+                birth_date = new Date(split_birth_date[2], split_birth_date[1]-1, split_birth_date[0])
+            }
+
+            var insert_object = { 
+                username: user_details.username,
+                first_name: user_details.first_name,
+                last_name: user_details.last_name,
+                email_address: user_details.email_address,
+                hashed_password: password_data.hashed_password, 
+                salt: password_data.salt,
+                d_o_b: birth_date,
+                img_title: user_details.img_title,
+                ip_addresses: ip_address ? [ ip_address ] : [ ],
+                date_created: new Date(),
+                last_seen: new Date(),
+                admin: user_details.admin,
+                registration_method: user_details.registration_method
+            };
+
+            var image_requires_download = true;
+            var file_buffer;
+
+            if(files && files[0]){
+                image_requires_download = false;
+                file_buffer = files[0];
+            }
+                
+            db_ref.get_db_object().connect(process.env.MONGODB_URI, function(err, db) {
+                
+                if(user_details.admin){ //if admin, check pending registered admin users table, to ensure a user hasnt previously requested admin registration with similar details
+
+                    db.collection(db_ref.get_pending_registered_admin_users_table()).aggregate([{ $match: { $or: [ { username: user_details.username}, { email_address: user_details.email_address } ] } }]).toArray(function(err, auth_arr){
+                        if(err){ console.log(err); }
+                        else{
+                            if(auth_arr.length > 0){
+                                if(auth_arr[0].username == user_details.username){
+                                    callback({failed: true, message: "Username is taken. (admin)"});
+                                }
+                                else{
+                                    callback({failed: true, message: "Email is taken. (admin)"});
+                                }
+                            }
+                            else{
+                                storage_interface.upload_image(image_requires_download, storage_ref.get_user_images_folder(), insert_object.img_title, files[0], false, function(img_title){
+
+                                    insert_object.img_title_fullsize = img_title;
+
+                                    storage_interface.upload_image(image_requires_download, storage_ref.get_user_images_folder(), insert_object.img_title, files[0], true, function(thumbnail_img_title){
+
+                                        insert_object.img_title_thumbnail = thumbnail_img_title;
+                                        //insert new user record
+                                        db.collection(db_ref.get_pending_registered_admin_users_table()).insert(insert_object, function(err, document){
+                                            if(err){ console.log(err); }
+                                            else{
+                                                callback({ user_id: document.ops[0]._id, message: "Registration complete, requires approval from an existing admin." });
+                                            }
+                                        });
+                                    });
+                                });
+                            }
+                        }
+                    });
+                }
+                else{
+
+                    storage_interface.upload_image(image_requires_download, storage_ref.get_user_images_folder(), insert_object.img_title, file_buffer, false, function(img_title){
+
+                        insert_object.img_title_fullsize = img_title;
+
+                        storage_interface.upload_image(image_requires_download, storage_ref.get_user_images_folder(), insert_object.img_title, file_buffer, true, function(thumbnail_img_title){
+
+                            insert_object.img_title_thumbnail = thumbnail_img_title;
+
+                            //add extra fields if not an admin user
+                            insert_object.viewed_beef_ids = [];
+                            insert_object.submitted_beef_ids = [];
+                            insert_object.submitted_actor_ids = [];
+                            insert_object.country = user_details.country;
+                            insert_object.contribution_score = 0;
+
+                            //insert new user record
+                            db.collection(db_ref.get_user_details_table()).insert(insert_object, function(err, document){
+                                if(err){ console.log(err); }
+                                else{
+                                    console.log(document);
+                                    callback({ user_id: document.ops[0]._id, message: "Registration complete."});
+                                }
+                            });
+                        });
+                    });
+                }
+            });
+        }
     },
     
     deleteUser: function(request, response, callback){
