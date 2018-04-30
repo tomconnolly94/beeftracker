5bafba0c9ba1df511113a9b07569a8cec56f7aab
Tom Connolly <tom.connolly@protonmail.com>
2018-04-30 22:13:38 +0100

    styling updates, added adsense code

--- server_files/routing/template_function_routing.js
+++ server_files/routing/template_function_routing.js
@@ -14,15 +14,11 @@ var build_template_function = function(request, response){
     
     //strip request.url to find the component name and its path
     var url_split = request.url.split("/");
-    console.log(url_split);
     var template_dir_name = url_split[1];
     var template_name = url_split[2].split("?")[0];
-    
     var template_path = template_dir + template_dir_name + "/" + template_name + ".jade";
     var function_name = template_name + "_tmpl_render_func";
-    
-    // Compile the template to a function string
-    var template_compilation_function = jade.compileFileClient(template_path, { exportMixins: true, name: function_name });
+    var template_compilation_function = jade.compileFileClient(template_path, { exportMixins: true, name: function_name }); //Compile the template to a function string
     
     if(template_compilation_function){
         response.status(200).send(template_compilation_function);
