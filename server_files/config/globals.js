4f6f870f587d6467dccde6cacfd8a29cf7d7c984
Tom Connolly <tom.connolly@protonmail.com>
2018-05-10 20:42:50 +0100

    hotfix

--- server_files/config/globals.js
+++ server_files/config/globals.js
@@ -1,5 +1,5 @@
 module.exports = {
     
-    file_server_url_prefix: "http://res.cloudinary.com/hghz4zts3/image/upload/v1514066941"
+    file_server_url_prefix: "https://res.cloudinary.com/hghz4zts3/image/upload/v1514066941"
     
 }
