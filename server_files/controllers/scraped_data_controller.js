9ae98339d7a4b1a6020c1c73ac68c8afc66656ef
Tom Connolly <tom.connolly@protonmail.com>
2018-05-17 12:41:05 +0100

    scraping dev

--- server_files/controllers/scraped_data_controller.js
+++ server_files/controllers/scraped_data_controller.js
@@ -7,12 +7,16 @@ var db_ref = require("../config/db_config.js");
 module.exports = {
 
     findScrapedEventData: function(query_parameters, callback){
+        
+        var limit = 10;
+        
+        if(query_parameters.limit) limit = query_parameters.limit;
 
         db_ref.get_db_object().connect(process.env.MONGODB_URI, function(err, db) {
             if(err){ console.log(err); }
             else{
                 
-                db.collection(db_ref.get_scraped_events_dump_table()).find({}).toArray(function(err, docs) {
+                db.collection(db_ref.get_scraped_events_dump_table()).find({}).limit(limit).toArray(function(err, docs) {
                     if(err){ console.log(err); }
                     else{
                         if(docs){
