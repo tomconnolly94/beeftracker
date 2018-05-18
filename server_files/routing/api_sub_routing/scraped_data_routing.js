52a3a42c47d3e960d5c9fec8a587dedd9f960ef5
Tom Connolly <tom.connolly@protonmail.com>
2018-05-18 19:39:20 +0100

    new routing files, scraping dev

---
+++ server_files/routing/api_sub_routing/scraped_data_routing.js
@@ -0,0 +1,41 @@
+ //external dependencies
+var express = require('express');
+var router = express.Router();
+
+//internal dependencies
+var scraped_data_controller = require('../../controllers/scraped_data_controller');
+var token_authentication = require("../../tools/token_authentication.js"); //get token authentication object
+var memoryUpload = require("../../config/multer_config.js").get_multer_object(); //get multer config
+var responses_object = require("./endpoint_response.js");
+
+//init response functions
+var send_successful_response = responses_object.send_successful_response;
+var send_unsuccessful_response = responses_object.send_unsuccessful_response;
+
+//Actor fields config endpoints
+router.route('/events').get(function(request, response){
+    scraped_data_controller.findScrapedEventData(function(data){
+        if(data.failed){
+            send_unsuccessful_response(response, 400, data.message);
+        }
+        else{
+            send_successful_response(response, 200, data);
+        }
+    });
+});//built, written, tested
+
+router.route('/events').delete(function(request, response){
+    
+    console.log(request.body);
+    
+    scraped_data_controller.deleteScrapedEventData(request.body, function(data){
+        if(data.failed){
+            send_unsuccessful_response(response, 400, data.message);
+        }
+        else{
+            send_successful_response(response, 200, data);
+        }
+    });
+});//built, written, tested
+
+module.exports = router;
