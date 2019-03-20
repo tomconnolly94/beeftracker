184a65d86736738146a570bb97e313f3f11ac655
Tom Connolly <tom.connolly@protonmail.com>
2019-03-20 20:05:37 +0100

    deactivated dotenv code

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const compression = require('compression');
 const validation_controller = require("./server_files/validation/validation_controller");
 const validator = require('express-validator');
 const fs = require('fs');
-const dotenv_result = require("dotenv").config();
-if (dotenv_result.error){ throw dotenv_result.error };
+//const dotenv_result = require("dotenv").config();
+//if (dotenv_result.error){ throw dotenv_result.error };
 
 // ## Sitemap generation ###
 sitemap = sitemap_generator.createSitemap ({
