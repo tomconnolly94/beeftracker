65b27aa0d0aba4018f7f9e27c695685677e483f9
Tom Connolly <tom.connolly@protonmail.com>
2019-04-15 22:55:09 +0200

    server hotfix, font adjustment

--- server_files/schemas/beef_chain.schema.js
+++ server_files/schemas/beef_chain.schema.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var beef_chain_schema = new Schema({
     actors : [ Schema.ObjectId ],
-    events : [ Schema.ObjectId ]
+    event_ids : [ Schema.ObjectId ]
 });
 
 module.exports = mongoose.model('BeefChain', beef_chain_schema);
